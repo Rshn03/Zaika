e4d14f563d3757bacd119ddebeb3142a4b887027
Rishin Mondal <rishinmondal092003@gmail.com>
2024-10-08 01:01:05 +0530

    added authentication, connected backend with frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,6 +6,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
+    const url="http://localhost:3000"    
+    const [token, setToken]=useState("")
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -32,7 +34,11 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
-
+    useEffect(()=>{
+        if (localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"))
+        }
+    },[])
 
     const contextValue = {
         food_list,
@@ -40,9 +46,12 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
-
+    
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
