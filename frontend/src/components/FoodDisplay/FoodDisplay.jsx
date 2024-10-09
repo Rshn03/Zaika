033af45760d268ff9a1c53dd020a3eb97324cd79
Rishin Mondal <rishinmondal092003@gmail.com>
2024-10-09 14:48:03 +0530

    created user order page

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({ category }) => {
 
-  const { food_list } = useContext(StoreContext);
+  const { food_list } = useContext(StoreContext)  
 
   return (
     <div className='food-display' id='food-display'>
@@ -13,7 +13,6 @@ const FoodDisplay = ({ category }) => {
       <div className="food-display-list">
         {food_list.map((item, index) => {
           if (category === "All" || category === item.category) {
-
             return <FoodItem
               key={index}
               id={item._id}
