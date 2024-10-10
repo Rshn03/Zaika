8c5fe18dd395f636eb1b58593ff6502664f6ccea
Rishin <154916812+Rshn03@users.noreply.github.com>
2024-10-10 14:24:33 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder= async(req,res)=>{
 
-	const frontend_url = "https://zaika-9ydf.onrender.com/";
+	const frontend_url = "https://localhost:5173";
 
 	try {
 		const newOrder= new orderModel({
